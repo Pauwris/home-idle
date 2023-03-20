cafb8375ffa1f84d8c4c0194923901d82e0acfaa
Kristof Pauwels <kpa@efficy.com>
2023-03-20 08:49:42 +0100

    Include SysTray + auto run on start

--- FormSettings.Designer.cs
+++ FormSettings.Designer.cs
@@ -31,8 +31,15 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSettings));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblIdleSeconds = new System.Windows.Forms.Label();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.contextMenuStripSysStray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIconSysTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStripSysStray.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -40,44 +47,95 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label1
+            // lblIdleSeconds
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblIdleSeconds.AutoSize = true;
+            this.lblIdleSeconds.Location = new System.Drawing.Point(12, 28);
+            this.lblIdleSeconds.Name = "lblIdleSeconds";
+            this.lblIdleSeconds.Size = new System.Drawing.Size(24, 15);
+            this.lblIdleSeconds.TabIndex = 0;
+            this.lblIdleSeconds.Text = "Off";
             // 
-            // button1
+            // btnStartStop
             // 
-            this.button1.Location = new System.Drawing.Point(24, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnStartStop.Location = new System.Drawing.Point(12, 46);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Start/Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
             // 
-            // Form1
+            // contextMenuStripSysStray
+            // 
+            this.contextMenuStripSysStray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStripSysStray.Name = "contextMenuStripSysStray";
+            this.contextMenuStripSysStray.Size = new System.Drawing.Size(181, 70);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.settingsToolStripMenuItem.Text = "Settings...";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIconSysTray
+            // 
+            this.notifyIconSysTray.BalloonTipTitle = "Home-idle";
+            this.notifyIconSysTray.ContextMenuStrip = this.contextMenuStripSysStray;
+            this.notifyIconSysTray.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconSysTray.Icon")));
+            this.notifyIconSysTray.Text = "Home-idle";
+            this.notifyIconSysTray.Visible = true;
+            this.notifyIconSysTray.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconSysTray_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblIdleSeconds);
+            this.groupBox1.Controls.Add(this.btnStartStop);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(118, 79);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Idle monitoring";
+            // 
+            // FormSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(440, 197);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "FormSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home-idle";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormConsole_FormClosing);
+            this.Resize += new System.EventHandler(this.FormSettings_Resize);
+            this.contextMenuStripSysStray.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private Label label1;
-        private Button button1;
+        private Label lblIdleSeconds;
+        private Button btnStartStop;
+        private ContextMenuStrip contextMenuStripSysStray;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private NotifyIcon notifyIconSysTray;
+        private GroupBox groupBox1;
     }
 }
--- FormSettings.cs
+++ FormSettings.cs
@@ -4,14 +4,60 @@ namespace home_idle
 {
     public partial class FormSettings : Form
     {
+        private bool allowVisible;     // ContextMenu's Show command used
+
         public FormSettings()
         {
             InitializeComponent();
+            setControlsState();
+            start();
+        }
+        public void start()
+        {
+            timer1.Enabled = true;
+            setControlsState();
+        }
+        public void stop()
+        {
+            timer1.Enabled = false;
+            setControlsState();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {            
             timer1.Enabled = !timer1.Enabled;
+            setControlsState();
+        }
+
+        private void FormSettings_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!allowVisible)
+            {
+                value = false;
+                if (!this.IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+        private void minizeToTray()
+        {
+            allowVisible = true;
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void FormConsole_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = (e.CloseReason != CloseReason.ApplicationExitCall);
+            if (e.Cancel == true)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -19,12 +65,41 @@ namespace home_idle
             BreakIdle breakIdle = new BreakIdle();
             int idleSeconds = breakIdle.secondsIdle();
 
-            if (idleSeconds > 5)
+            if (idleSeconds > 59)
             {
                 breakIdle.resetIdle();
             }
 
-            label1.Text = idleSeconds.ToString();
+            lblIdleSeconds.Text = idleSeconds.ToString();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            minizeToTray();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIconSysTray_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            minizeToTray();
+        }
+
+        private void setControlsState()
+        {
+            if (timer1.Enabled)
+            {
+                btnStartStop.Text = "Stop";
+                lblIdleSeconds.Text = "0";
+            }
+            else
+            {
+                btnStartStop.Text = "Start";
+                lblIdleSeconds.Text = "Off"; 
+            }
         }
     }
 }
